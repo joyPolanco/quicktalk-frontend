6e039e4475b7adc1684efd771856ab9f5072e08f
Johaly <johalypolanco13@gmail.com>
2026-05-06 06:31:34 -0400

    deploy

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
+import { CLIENT_URL } from '../../../backend/config/env.js';
 
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
-    : "/api",
+    : CLIENT_URL+"/api",
   withCredentials: true
 });
